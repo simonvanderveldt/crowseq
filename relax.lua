b6eceaec50e8186621720964bf5c5ce0876692a6
Simon van der Veldt <simon.vanderveldt@gmail.com>
2020-04-14 22:30:58 +0200

    Move all track properties to tracks table
    To prepare for multiple tracks

--- relax.lua
+++ relax.lua
@@ -23,26 +23,31 @@ local grid_buttons_pressed = {}
 local pages = {pitch = 1, offset = 2}
 local page = "pitch"
 
--- Track properties
-local pitch = {}
-pitch.pitches = {}
-pitch.triggers = {}
-pitch.position = 1
-pitch.loop_start = 1
-pitch.loop_end = 16
-pitch.transpose = {}
-local offset = {}
-offset.pitches = {}
-offset.triggers = {}
-offset.position = 1
-offset.loop_start = 1
-offset.loop_end = 16
+-- Tracks
+tracks = {}
+-- for i = 1,3 do
+tracks[1] = {}
+tracks[1].pitch = {}
+tracks[1].pitch.pitches = {}
+tracks[1].pitch.triggers = {}
+tracks[1].pitch.position = 1
+tracks[1].pitch.loop_start = 1 -- or start_point and end_point?
+tracks[1].pitch.loop_end = 16
+tracks[1].pitch.transpose = {}
+tracks[1].offset = {}
+tracks[1].offset.pitches = {}
+tracks[1].offset.triggers = {}
+tracks[1].offset.position = 1
+tracks[1].offset.loop_start = 1
+tracks[1].offset.loop_end = 16
+-- end
+
 
 local task_id = nil
 local playback_icon = UI.PlaybackIcon.new(121, 55)
 
 -- Create a fake local table just like _G
-_L = {pitch = pitch, offset = offset}
+-- _L = {pitch = pitch, offset = offset}
 
 -- mode = math.random(#music.SCALES)
 -- TU.print(music.SCALES)
@@ -51,11 +56,11 @@ scale = music.generate_scale_of_length(8, "major", 14)
 
 function init()
   for i=1,16 do
-    table.insert(pitch.pitches, math.random(7))
-    table.insert(pitch.triggers, true)
-    table.insert(pitch.transpose, 0)
-    table.insert(offset.pitches, 0)
-    table.insert(offset.triggers, false)
+    table.insert(tracks[1].pitch.pitches, math.random(7))
+    table.insert(tracks[1].pitch.triggers, true)
+    table.insert(tracks[1].pitch.transpose, 0)
+    table.insert(tracks[1].offset.pitches, 0)
+    table.insert(tracks[1].offset.triggers, false)
   end
   grid_redraw()
 
@@ -84,27 +89,27 @@ function step()
   while true do
     clock.sync(1/divisor)
     if running then
-      -- print(pitch.position)
-      -- print(pitch.pitches[pitch.position])
-      if pitch.triggers[pitch.position] then
-        local note_num = 8 - pitch.pitches[pitch.position]
-        if offset.triggers[offset.position] then
-          note_num = note_num + (8 - offset.pitches[offset.position])
+      -- print(tracks[1].pitch.position)
+      -- print(tracks[1].pitch.pitches[tracks[1].pitch.position])
+      if tracks[1].pitch.triggers[tracks[1].pitch.position] then
+        local note_num = 8 - tracks[1].pitch.pitches[tracks[1].pitch.position]
+        if tracks[1].offset.triggers[tracks[1].offset.position] then
+          note_num = note_num + (8 - tracks[1].offset.pitches[tracks[1].offset.position])
         end
         local note_value = scale[note_num]/12
-        -- print(pitch.transpose[pitch.position])
+        -- print(tracks[1].pitch.transpose[tracks[1].pitch.position])
         -- print(note_num)
-        crow.output[2].volts = note_value + pitch.transpose[pitch.position]
+        crow.output[2].volts = note_value + tracks[1].pitch.transpose[tracks[1].pitch.position]
         crow.output[1].execute()
         -- grid_redraw()
       end
-      -- print(pitch.position)
-      -- pitch.position = util.clamp(pitch.position + 1, pitch.loop_start, pitch.loop_end)
-      -- Increment pitch.position. Wrap pitch.loop_start in case the current pitch.position is pitch.loop_end
-      pitch.position = math.max((pitch.position % pitch.loop_end) + 1, pitch.loop_start)
-      offset.position = math.max((offset.position % offset.loop_end) + 1, offset.loop_start)
-      -- print(pitch.position)
-      -- print(offset.position)
+      -- print(tracks[1].pitch.position)
+      -- tracks[1].pitch.position = util.clamp(tracks[1].pitch.position + 1, tracks[1].pitch.loop_start, tracks[1].pitch.loop_end)
+      -- Increment tracks[1].pitch.position. Wrap tracks[1].pitch.loop_start in case the current tracks[1].pitch.position is tracks[1].pitch.loop_end
+      tracks[1].pitch.position = math.max((tracks[1].pitch.position % tracks[1].pitch.loop_end) + 1, tracks[1].pitch.loop_start)
+      tracks[1].offset.position = math.max((tracks[1].offset.position % tracks[1].offset.loop_end) + 1, tracks[1].offset.loop_start)
+      -- print(tracks[1].pitch.position)
+      -- print(tracks[1].offset.position)
     end
   end
 end
@@ -113,8 +118,8 @@ function key(n,z)
   if n == 2 and z == 1 then
       -- clock.cancel(task_id)
       -- task_id = clock.run(step)
-      pitch.position = 1
-      offset.position = 1
+      tracks[1].pitch.position = 1
+      tracks[1].offset.position = 1
       if not running then
         playback_icon.status = 4
         -- I don't like this, maybe grid_redraw should also be run as a corouting, just like redraw?
@@ -143,22 +148,22 @@ function enc(n,d)
   elseif n == 3 then
     -- Transpose
     -- This is wrong/upside down. Not sure what the best way to "flip" the grid is
-    -- TU.print(pitch.transpose)
-    for k, v in pairs(pitch.pitches) do
+    -- TU.print(tracks[1].pitch.transpose)
+    for k, v in pairs(tracks[1].pitch.pitches) do
       if v ~= 0 then
         local new_v = v - d
         if new_v < 1 then
-          pitch.pitches[k] = 7
-          pitch.transpose[k] = util.clamp(pitch.transpose[k] + 1, -3, 3)
+          tracks[1].pitch.pitches[k] = 7
+          tracks[1].pitch.transpose[k] = util.clamp(tracks[1].pitch.transpose[k] + 1, -3, 3)
         elseif new_v > 7 then
-          pitch.pitches[k] = 1
-          pitch.transpose[k] = util.clamp(pitch.transpose[k] - 1, -3, 3)
+          tracks[1].pitch.pitches[k] = 1
+          tracks[1].pitch.transpose[k] = util.clamp(tracks[1].pitch.transpose[k] - 1, -3, 3)
         else
-          pitch.pitches[k] = new_v
+          tracks[1].pitch.pitches[k] = new_v
         end
       end
-      -- print(k, pitch.pitches[k])
-      -- TU.print(pitch.transpose)
+      -- print(k, tracks[1].pitch.pitches[k])
+      -- TU.print(tracks[1].pitch.transpose)
       -- grid_redraw()
     end
   end
@@ -172,7 +177,7 @@ function redraw()
   screen.move(0, 14)
   screen.line(128, 14)
   screen.stroke()
-  -- screen.text("transp: "..pitch.transpose)
+  -- screen.text("transp: "..tracks[1].pitch.transpose)
   playback_icon:redraw()
   screen.update()
 end
@@ -194,8 +199,8 @@ g.key = function(x,y,z)
       -- pitch/keys with being able to pick loop start and end points?
       if #grid_buttons_pressed == 1 then
         grid_buttons_pressed[2] = x
-        _L[page].loop_start = math.min(grid_buttons_pressed[1], x)
-        _L[page].loop_end = math.max(grid_buttons_pressed[1], x)
+        tracks[1][page].loop_start = math.min(grid_buttons_pressed[1], x)
+        tracks[1][page].loop_end = math.max(grid_buttons_pressed[1], x)
         grid_buttons_pressed = {}
       else
         grid_buttons_pressed[1] = x
@@ -205,21 +210,21 @@ g.key = function(x,y,z)
       if TU.contains(grid_buttons_pressed, x) then
         -- if y == 8 then -- control row
         --   -- print(x)
-        if _L[page].triggers[x] and _L[page].pitches[x] == y then
+        if tracks[1][page].triggers[x] and tracks[1][page].pitches[x] == y then
           -- Existing note pressed, turn off step
-          _L[page].triggers[x] = false
+          tracks[1][page].triggers[x] = false
         else
           -- New note pressed
-          _L[page].pitches[x] = y
-          _L[page].triggers[x] = true
+          tracks[1][page].pitches[x] = y
+          tracks[1][page].triggers[x] = true
         end
         -- grid_redraw()
-        -- TU.print(_L[page].triggers)
+        -- TU.print(tracks[1][page].triggers)
         table.remove(grid_buttons_pressed, TU.key(grid_buttons_pressed, x))
       end
     end
   end
-  -- TU.print(offset.triggers)
+  -- TU.print(tracks[1].offset.triggers)
   -- TU.print(offset)
 end
 
@@ -227,13 +232,13 @@ function grid_redraw()
   g:all(0)
   for i=1,16 do
     local low_value = 7
-    if i < _L[page].loop_start or i > _L[page].loop_end then
+    if i < tracks[1][page].loop_start or i > tracks[1][page].loop_end then
       low_value = 4
     end
-    if _L[page].triggers[i] then
-      g:led(i,_L[page].pitches[i],i==_L[page].position and 15 or low_value)
+    if tracks[1][page].triggers[i] then
+      g:led(i,tracks[1][page].pitches[i],i==tracks[1][page].position and 15 or low_value)
     else
-      g:led(i,1,i==_L[page].position and 3 or 0)
+      g:led(i,1,i==tracks[1][page].position and 3 or 0)
     end
   end
   for k, v in pairs(pages) do
